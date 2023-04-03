f80409fb026419859587985ca92c4b584d53dd58
pavan kumar <pavanmohan.ce@gmail.com>
2023-04-03 17:40:46 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region="us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    key            = "demo/rds/terraform.tfstate"
+    region         = "us-east-1"
+    bucket         = "pavan-us-east-1-tfstate"
+    encrypt        = true
+  }
+}
